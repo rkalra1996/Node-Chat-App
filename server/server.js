92cee4576c1b115fefa911a2e2ecd32edc0e8f38
Rishabh Kalra <Rishabh.Kalra@niit.com>
2019-02-05 14:23:04 +0530

    created socket connections

--- server/server.js
+++ server/server.js
@@ -1,17 +1,29 @@
 const path = require('path');
+const http = require('http');
 //specific path to the public directory
 const publicPath = path.join(__dirname, '../public');
 
 const express = require('express');
 const app = express();
 
+const socketIO = require('socket.io');
+
 const port = process.env.PORT || 3000
+
+const server = http.createServer(app);
+//create a web-socket basesd server
+const client = socketIO(server);
 
 app.use(express.static(publicPath));
 
 
+//listen to socket connection
+client.on('connection', (socket)=>{
+    console.warn("A new socket is now active");
+});
 
+client.on('disconnect', () => {console.log("user disconnected")});
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`app active on port ${port}`);
 });
