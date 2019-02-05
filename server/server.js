dd22ef21c052d2d7473edd9b9e4bdb17c69422d7
Rishabh Kalra <Rishabh.Kalra@niit.com>
2019-02-05 13:34:46 +0530

    setting up the initial server

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
-ECHO is on.
+const path = require('path');
+//specific path to the public directory
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use(express.static(publicPath));
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`app active on port ${port}`);
+});
